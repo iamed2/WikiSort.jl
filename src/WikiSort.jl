81561f598c6fef5981d40513ed227000a31a36c3
Eric Davies <iamed2@gmail.com>
2016-01-24 13:15:56 -0600

    Up to rotation tests.

--- src/WikiSort.jl
+++ src/WikiSort.jl
@@ -1,5 +1,127 @@
 module WikiSort
 
-# package code goes here
+const DEFAULT_CACHE_SIZE = 512
+
+immutable WikiSorter{T}
+    cache::Vector{T}
+    cache_size::Int
+end
+
+function call{T}(::Type{WikiSorter{T}}, cache_size::Integer=DEFAULT_CACHE_SIZE)
+    return WikiSorter{T}(fill(zero(T), cache_size), cache_size)
+end
+
+function reverse!(arr::AbstractVector, r::UnitRange)
+    for i = (div(length(r), 2) - 1):-1:0
+        arr[r.start + i], arr[r.stop - i] = arr[r.stop - i], arr[r.start + i]
+    end
+end
+
+function block_swap!(arr::AbstractVector, start_idx1::Integer, start_idx2::Integer, count::Integer)
+    for i = 1:count
+        # swap
+        arr[start_idx1 + i], arr[start_idx2 + i] = arr[start_idx2 + i], arr[start_idx1 + i]
+    end
+end
+
+# rotate the values in an array ([0 1 2 3] becomes [1 2 3 0] if we rotate by 1)
+# this assumes that 0 <= amount <= length(r)
+function rotate!{T}(s::WikiSorter{T}, arr::AbstractVector{T}, r::UnitRange, amount::Integer, use_cache=true)
+    if length(r) == 0
+        return
+    end
+
+    if amount >= 0
+        split = r.start + amount
+    else
+        split = r.stop + amount + 1
+    end
+
+    range1 = r.start:(split - 1)
+    range2 = split:r.stop
+
+    if use_cache
+        if length(range1) <= length(range2)
+            if length(range1) <= s.cache_size
+                # if cache not null
+                # unsafe_copy!
+                copy!(s.cache, 1, arr, range1.start, length(range1))
+                copy!(arr, range1.start, arr, range2.start, length(range2))
+                copy!(arr, range1.start + length(range2), s.cache, 1, length(range1))
+                return
+            end
+        else
+            if length(range2) <= s.cache_size
+                # ditto
+                copy!(s.cache, 1, arr, range2.start, length(range2))
+                copy!(arr, range2.stop - length(range1) + 1, arr, range1.start, length(range1))
+                copy!(arr, range1.start, s.cache, 1, length(range2))
+                return
+            end
+        end
+    end
+
+    reverse!(arr, range1)
+    reverse!(arr, range2)
+    reverse!(arr, r)
+    return
+end
+
+function rotate!(arr::AbstractVector, r::UnitRange, amount::Integer)
+    Base.reverse!(arr, r.start, r.start + amount - 1)
+    Base.reverse!(arr, r.start + amount, r.stop)
+    Base.reverse!(arr, r.start, r.stop)
+end
+
+function binaryfirst(arr::AbstractVector, r::UnitRange, value)
+    start = r.start
+    stop = r.stop
+    while start < stop
+        mid = start + div(stop - start, 2)
+        if arr[mid] < value
+            start = mid + 1
+        else
+            stop = mid
+        end
+    end
+
+    if start == r.stop && arr[start] < value
+        start += 1
+    end
+
+    return start
+end
+
+function binarylast(arr::AbstractVector, r::UnitRange, value)
+    start = r.start
+    stop = r.stop
+    while start < stop
+        mid = start + div(stop - start, 2)
+        if arr[mid] <= value
+            start = mid + 1
+        else
+            stop = mid
+        end
+    end
+
+    if start == r.stop && arr[start] <= value
+        start += 1
+    end
+
+    return start
+end
+
+function insertion_sort!(arr::AbstractVector, r::UnitRange)
+    for i = (r.start + 1):r.stop
+        temp = arr[i]
+        j = i
+        while j > r.start && arr[j - 1] > temp
+            arr[j] = arr[j - 1]
+            j -= 1
+        end
+        arr[j] = temp
+    end
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,247 @@
 using WikiSort
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+
+@testset "Reverse" begin
+    @testset "Whole array" begin
+        reverse_tests = [
+            (Int[], Int[]),
+            ([1], [1]),
+            ([1, 2], [2, 1]),
+            ([1, 2, 3], [3, 2, 1]),
+            (collect(1:1000), collect(1000:-1:1)),
+        ]
+
+        @testset "Base" for (fwd, rev) in reverse_tests
+            fwd = copy(fwd)
+            Base.reverse!(fwd, 1, length(fwd))
+            @test fwd == rev
+        end
+
+        @testset "WikiSort" for (fwd, rev) in reverse_tests
+            fwd = copy(fwd)
+            WikiSort.reverse!(fwd, 1:length(fwd))
+            @test fwd == rev
+        end
+    end
+
+    @testset "Partial reverse" begin
+        fwd = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        reverse_tests = [
+            (1:0, fwd),
+            (1:1, fwd),
+            (5:4, fwd),
+            (5:5, fwd),
+            (9:8, fwd),
+            (9:9, fwd),
+            (1:2, [2, 1, 3, 4, 5, 6, 7, 8, 9]),
+            (1:3, [3, 2, 1, 4, 5, 6, 7, 8, 9]),
+            (4:6, [1, 2, 3, 6, 5, 4, 7, 8, 9]),
+            (5:9, [1, 2, 3, 4, 9, 8, 7, 6, 5]),
+        ]
+
+        @testset "Base" for (rng, rev) in reverse_tests
+            fwd_copy = copy(fwd)
+            Base.reverse!(fwd_copy, rng.start, rng.stop)
+            @test fwd_copy == rev
+        end
+
+        @testset "WikiSort" for (rng, rev) in reverse_tests
+            fwd_copy = copy(fwd)
+            WikiSort.reverse!(fwd_copy, rng)
+            @test fwd_copy == rev
+        end
+    end
+end
+
+@testset "Rotate" begin
+    ws = WikiSort.WikiSorter{Int}()
+
+    @testset "Full rotation" begin
+        @testset "Full array" begin
+            rotation_tests = [
+                (Int[], Int[]),
+                ([1], [1]),
+                ([2, 3], [2, 3]),
+                (collect(1:10), collect(1:10)),
+            ]
+
+            @testset "Forward" begin
+                @testset "No cache" for (orig, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(orig_copy)
+                    WikiSort.rotate!(ws, orig_copy, 1:amt, amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(orig_copy)
+                    WikiSort.rotate!(ws, orig_copy, 1:amt, amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+
+            @testset "Reverse" begin
+                @testset "No cache" for (orig, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(orig_copy)
+                    WikiSort.rotate!(ws, orig_copy, 1:amt, -amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(orig_copy)
+                    WikiSort.rotate!(ws, orig_copy, 1:amt, -amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+        end
+
+        @testset "Partial array" begin
+            arr = collect(1:10)
+
+            rotation_tests = [
+                ([1], 1:0, [1]),
+                ([1], 2:1, [1]),
+                ([2, 3], 1:1, [2, 3]),
+                ([2, 3], 2:2, [2, 3]),
+                (arr, 1:9, arr),
+                (arr, 5:9, arr),
+                (arr, 8:9, arr),
+                (arr, 2:10, arr),
+                (arr, 7:10, arr),
+                (arr, 9:10, arr),
+            ]
+
+            @testset "Forward" begin
+                @testset "No cache" for (orig, rng, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(rng)
+                    WikiSort.rotate!(ws, orig_copy, rng, amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, rng, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(rng)
+                    WikiSort.rotate!(ws, orig_copy, rng, amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+
+            @testset "Reverse" begin
+                @testset "No cache" for (orig, rng, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(rng)
+                    WikiSort.rotate!(ws, orig_copy, rng, -amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, rng, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    amt = length(rng)
+                    WikiSort.rotate!(ws, orig_copy, rng, -amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+        end
+    end
+
+    @testset "Partial rotation" begin
+        @testset "Full array" begin
+            @testset "Forward" begin
+                rotation_tests = [
+                    ([2, 3], 1, [3, 2]),
+                    ([1, 2, 3], 1, [2, 3, 1]),
+                    ([1, 2, 3], 2, [3, 1, 2]),
+                ]
+
+                @testset "No cache" for (orig, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, 1:length(orig_copy), amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, 1:length(orig_copy), amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+
+            @testset "Reverse" begin
+                rotation_tests = [
+                    ([2, 3], -1, [3, 2]),
+                    ([1, 2, 3], -1, [3, 1, 2]),
+                    ([1, 2, 3], -2, [2, 3, 1]),
+                ]
+
+                @testset "No cache" for (orig, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, 1:length(orig_copy), amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, 1:length(orig_copy), amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+        end
+
+        @testset "Partial array" begin
+            arr = collect(1:10)
+
+            @testset "Forward" begin
+                rotation_tests = [
+                    ([1, 2, 3, 4], 1:2, 1, [2, 1, 3, 4]),
+                    ([1, 2, 3, 4], 2:3, 1, [1, 3, 2, 4]),
+                    ([1, 2, 3, 4], 3:4, 1, [1, 2, 4, 3]),
+                    ([1, 2, 3, 4], 1:3, 1, [2, 3, 1, 4]),
+                    ([1, 2, 3, 4], 1:3, 2, [3, 1, 2, 4]),
+                    ([1, 2, 3, 4], 2:4, 1, [1, 3, 4, 2]),
+                    ([1, 2, 3, 4], 2:4, 2, [1, 4, 2, 3]),
+                ]
+
+                @testset "No cache" for (orig, rng, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, rng, amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, rng, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, rng, amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+
+            @testset "Reverse" begin
+                rotation_tests = [
+                    ([1, 2, 3, 4], 1:2, -1, [2, 1, 3, 4]),
+                    ([1, 2, 3, 4], 2:3, -1, [1, 3, 2, 4]),
+                    ([1, 2, 3, 4], 3:4, -1, [1, 2, 4, 3]),
+                    ([1, 2, 3, 4], 1:3, -1, [3, 1, 2, 4]),
+                    ([1, 2, 3, 4], 1:3, -2, [2, 3, 1, 4]),
+                    ([1, 2, 3, 4], 2:4, -1, [1, 4, 2, 3]),
+                    ([1, 2, 3, 4], 2:4, -2, [1, 3, 4, 2]),
+                ]
+
+                @testset "No cache" for (orig, rng, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, rng, amt, false)
+                    @test orig_copy == rotd
+                end
+
+                @testset "Cache" for (orig, rng, amt, rotd) in rotation_tests
+                    orig_copy = copy(orig)
+                    WikiSort.rotate!(ws, orig_copy, rng, amt, true)
+                    @test orig_copy == rotd
+                end
+            end
+        end
+    end
+end
